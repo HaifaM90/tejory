d2434422d32ae3969f6b731fcadeb60bb2ab43f0
HaifaM90 <haifa@tamkeentech.sa>
2018-08-02 20:35:25 +0300

    app

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { StyleSheet, View} from 'react-native'
 import SignIn from './containers/SignIn'
 import Registration from './containers/Registration'
 import Dashboard from './containers/Dashboard'
+import AccountStatement from './containers/AccountStatement'
 
 export default class App extends React.Component {
 
  render() {
    return (
      <View style={styles.container}>
-         <Dashboard />
+        <AccountStatement/>
      </View>
    );
  }
